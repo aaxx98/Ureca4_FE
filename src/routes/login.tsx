817df11b7a7d0f874cc8b7c2911f046a0bf40a69
Paul Yang <sea5122916@gmail.com>
2026-03-16 14:07:43 +0900

    fix: 리프레시 토큰

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,6 +1,13 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { LoginPage } from "../pages/login/LoginPage";
+import { getAccessToken } from "../shared/api/tokenStore";
+import { ROUTES } from "../shared/config/routes";
 
 export const Route = createFileRoute("/login")({
+  beforeLoad: () => {
+    if (getAccessToken()) {
+      throw redirect({ to: ROUTES.HOME });
+    }
+  },
   component: LoginPage,
 });
